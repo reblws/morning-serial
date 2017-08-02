c63ec560adb45760e8537589d10a7aca9466cfd1
Enrico B <rflnrclws@protonmail.com>
2017-08-02 16:22:34 -0400

    Add functions for interacting with db

--- src/db/index.js
+++ src/db/index.js
@@ -1,33 +1,41 @@
 const r = require('rethinkdb');
 const data = require('../data');
+const config = require('./config');
+const { formatType } = require('../utils');
 
 function logChanges(err, result) {
   if (err) throw err;
   console.log(JSON.stringify(result, null, 2));
 }
 
+const connection = r.connect(config);
 
-// Open the connection
-r.connect({ host: 'localhost', port: 28015 })
-  .then(conn => {
-    // r.db('test').tableCreate('articles').run(conn, logChanges)
-    r.db('test').table('articles').indexCreate('type').run(conn, logChanges);
-  })
-  /* Basic insert */
-  // .then(async conn => {
-  //   const table = r.db('test').table('articles');
-  //   const feed = await data.DesignerNews.listing;
-  //   // Strip the feed of everything that's already in there
-  //   const filteredFeed = feed.
-  //   table.insert(feed).run(conn, logChanges);
-  // });
+// Tables need to have no dash
+function setupTable(conn, table) {
+  return r.tableCreate(table, { primaryKey: 'uuid'}).run(conn);
+}
 
+async function updateTable(conn, feedType, documents) {
+  // Check if the table exists
+  const feedTable = formatType(feedType);
+  const availableTables = await r.tableList().run(conn);
+  if (!availableTables.includes(feedTable)) {
+    setupTable(conn, feedTable);
+  }
+  const documentsToInsert = documents.map(uuids(conn));
 
-  .then(async conn => {
-    const table = r.db('test').table('articles');
-    const feed = await data.DesignerNews.listing;
-  })
+  return r.table(feedTable).insert(documentsToInsert).run(conn);
+}
 
+// Map function for adding uuids to documents before table
+// insert
+async function uuids(conn) {
+  return async function addUUID(doc) {
+    const { type, link, date } = doc;
+    const uuid = await promiseUniqueIdentifier(type, link, date, conn);
+    return Object.assign({}, doc, { uuid });
+  }
+}
 
 /**
  * Returns a function that creates a unique identifier for a given set of keys.
@@ -36,9 +44,13 @@ r.connect({ host: 'localhost', port: 28015 })
  * @param {any} connection - Instance of connection to rethinkdb
  * @returns {Function}
  */
-function promiseUniqueIdentifier(r, connection) {
+function promiseUniqueIdentifier(type, link, date, conn) {
   const compositeString = type + link + Date.parse(date);
-  return (type, link, date) => r.uuid(compositeString).run(connection);
+  return r.uuid(compositeString).run(conn);
 }
 
-module.exports = r;
+module.exports = {
+  connection,
+  setupTable,
+  updateTable,
+};

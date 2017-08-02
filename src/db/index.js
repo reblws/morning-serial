73bc132b2b00f59bc1099209eebffd1c84201cd4
Enrico B <rflnrclws@protonmail.com>
2017-08-02 16:55:32 -0400

    Add new functions for assigning uuids to documents Avoids relying on an async map function, which doesn't work.

--- src/db/index.js
+++ src/db/index.js
@@ -12,7 +12,7 @@ const connection = r.connect(config);
 
 // Tables need to have no dash
 function setupTable(conn, table) {
-  return r.tableCreate(table, { primaryKey: 'uuid'}).run(conn);
+  return r.tableCreate(table, { primaryKey: 'uuid' }).run(conn);
 }
 
 async function updateTable(conn, feedType, documents) {
@@ -20,22 +20,35 @@ async function updateTable(conn, feedType, documents) {
   const feedTable = formatType(feedType);
   const availableTables = await r.tableList().run(conn);
   if (!availableTables.includes(feedTable)) {
-    setupTable(conn, feedTable);
+    setupTable(conn, feedTable).then(results => console.log(results));
   }
-  const documentsToInsert = documents.map(uuids(conn));
-
+  const uuids = await Promise.all(promiseUUIDs(documents, conn));
+  const documentsToInsert = documents.map(mergeUUIDs(uuids, conn));
   return r.table(feedTable).insert(documentsToInsert).run(conn);
+}
+
+function mergeUUIDs(uuids, conn) {
+  return (doc, index) => Object.assign(doc, { uuid: uuids[index]});
+}
+
+function promiseUUIDs(documents, conn) {
+  return documents.map(({ type, link, date }) => {
+    return promiseUniqueIdentifier(type, link, date, conn);
+  });
 }
 
 // Map function for adding uuids to documents before table
 // insert
-async function uuids(conn) {
-  return async function addUUID(doc) {
-    const { type, link, date } = doc;
-    const uuid = await promiseUniqueIdentifier(type, link, date, conn);
-    return Object.assign({}, doc, { uuid });
-  }
-}
+/* This is problematic. It's not good to combine map functions
+  with async methods
+*/
+// async function uuids(conn) {
+//   return async function addUUID(doc) {
+//     const { type, link, date } = doc;
+//     const uuid = await promiseUniqueIdentifier(type, link, date, conn);
+//     return Object.assign({}, doc, { uuid });
+//   }
+// }
 
 /**
  * Returns a function that creates a unique identifier for a given set of keys.

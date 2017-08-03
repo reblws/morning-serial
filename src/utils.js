edf62766842d44d8f190915286f5f9293b73c5f6
Enrico B <rflnrclws@protonmail.com>
2017-08-03 19:17:46 -0400

    Add toHumanName and hostName fns

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,18 @@
+const url = require('url');
+
+function capitalizeFirstLetter(letter, index) {
+  return index === 0
+    ? letter.toUpperCase()
+    : letter;
+}
+
+function capitalize(str) {
+  return str.split('').map(capitalizeFirstLetter).join('');
+}
+
 module.exports = {
+  toHumanName: type => type.split('-').map(capitalize).join(' '),
   toTableName: type => type.replace(/\W/g, ''),
+  hostName: href => url.parse(href).host,
   valueSeq: object => Object.keys(object).map(key => object[key]),
 }

504e29bb9829a91f850023d3df00b33e630ca5eb
Enrico B <rflnrclws@protonmail.com>
2017-08-03 15:00:49 -0400

    'check' --> 'update', change valueSeq import

--- src/worker.js
+++ src/worker.js
@@ -1,31 +1,27 @@
 const data = require('./data');
 const Feed = require('./data/sources/feed');
 const db = require('./db');
-const valueSeq = require('./utils/value-seq');
+const { valueSeq } = require('./utils');
 
 // Just test one of the sources first
 const sources = valueSeq(data);
 
-// Iterate over sources and checkFeeds for each
-function checkAllFeeds(sources, db) {
+// Iterate over sources and updateFeeds for each
+function updateAllFeeds(sources, db) {
   // Check only ATOM/RSS feeds for now
   const feeds = sources.filter(source => Feed.prototype.isPrototypeOf(source));
-  feeds.forEach(source => {
-    checkFeeds(source, db);
-  });
+  feeds.forEach(feed => updateFeed(feed, db));
 };
 
 
-function checkFeeds(source, db) {
+function updateFeed(feed, db) {
   return db.connection.then(async conn => {
     try {
-      const docs = await source.listing;
-      return db.updateTable(conn, source.type, docs)
-        .then(results => console.log(results, source.type));
+      const docs = await feed.listing;
+      return db.updateTable(conn, feed.type, docs)
+        .then(results => console.log(results, feed.type));
     } catch(e) {
       throw e;
     }
   });
 }
-
-checkAllFeeds(sources, db);

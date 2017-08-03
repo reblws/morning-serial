306c82f03e335ec0b91bc6daab135834e4621e6c
Enrico B <rflnrclws@protonmail.com>
2017-08-03 19:18:21 -0400

    Add endpoint for showing all possible sources

--- src/server.js
+++ src/server.js
@@ -3,20 +3,20 @@ const bodyParser = require('body-parser');
 // const axios = require('axios');
 // const jsonfile = require('jsonfile');
 const { connection, readTables } = require('./db');
-
+const data = require('./data');
+const { valueSeq } = require('./utils');
 const app = express();
 const PORT = process.env.PORT || 9999;
 
-const data = require('./data');
-const types = require('./data/types');
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.get('/feeds', async (request, response) => {
+app.get('/api/feeds', async (request, response) => {
   const { sources } = request.query;
   // parse the query
+  // Going to assume they are the string values from ./data/types
   const feeds = sources.split(/\s/);
   try {
     const conn = await connection;
@@ -26,6 +26,14 @@ app.get('/feeds', async (request, response) => {
     response.sendStatus(500);
     throw e;
   }
+});
+
+// Show the available sources
+app.get('/api/sources', (_, response) => {
+  const sources = valueSeq(data);
+  response.json(
+    sources.map(({ name, faviconURL, host }) => ({ name, faviconURL, host })),
+  );
 });
 
 app.listen(PORT);

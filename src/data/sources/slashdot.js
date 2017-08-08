3d790d717c3f3759a1e4295a2a9db8f0855c6684
Enrico B <rflnrclws@protonmail.com>
2017-08-08 12:46:44 -0400

    Fix slashdot favicon

--- src/data/sources/slashdot.js
+++ src/data/sources/slashdot.js
@@ -1,6 +1,6 @@
 const Feed = require('./feed');
 const types = require('../types');
 
-const Slashdot = new Feed('http://rss.slashdot.org/Slashdot/slashdotMain', types.Slashdot, 'http://macrumors.com');
+const Slashdot = new Feed('http://rss.slashdot.org/Slashdot/slashdotMain', types.Slashdot, 'http://slashdot.org');
 
 module.exports = Slashdot;

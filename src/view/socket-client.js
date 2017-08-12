ea3eb5d2dcb087b7428e87ef43e3e556ae3cd0a7
Enrico B <rflnrclws@protonmail.com>
2017-08-12 17:04:36 -0400

    Add socket-client class

---
+++ src/view/socket-client.js
@@ -0,0 +1,39 @@
+import io from 'socket.io-client';
+
+class SocketClient {
+  constructor() {
+    this.socket = null;
+  }
+
+  // Opens a socket io connections
+  open(rooms, callback) {
+    this.socket = io('localhost:9000');
+    this.socket.emit('i want to join', rooms);
+    this.socket.on('new article', callback);
+  }
+
+  leave(room) {
+    if (!this.socket) {
+      throw new Error("Can't leave room, socket not opened");
+    }
+    this.socket.emit('i want to leave', room);
+  }
+
+  join(room) {
+    if (!this.socket) {
+      throw new Error("Can't join room, socket not opened.")
+    }
+    this.socket.emit('i want to join', room);
+  }
+
+  close() {
+    this.socket.disconnect();
+    this.socket = null;
+  }
+
+  exists() {
+    return this.socket !== null;
+  }
+}
+
+export default new SocketClient();

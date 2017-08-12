af4ef5fd9c6b818fa7875598a35a0ca441d636f0
Enrico B <rflnrclws@protonmail.com>
2017-08-12 02:45:21 -0400

    Separate middleware

---
+++ src/middleware.js
@@ -0,0 +1,40 @@
+/* Middleware Imports */
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const csp = require('helmet-csp');
+
+module.exports = app => {
+  // Middleware
+  const cspSettings = csp({
+    directives: {
+      defaultSrc: [
+        "'self'",
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com',
+      ],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+      ],
+      imgSrc: [
+        '*',
+      ],
+      styleSrc: [
+        "'self'",
+        'https://fonts.gstatic.com',
+        'https://fonts.googleapis.com',
+        "'unsafe-inline'",
+      ],
+      connectSrc: [
+        "'self'",
+        'ws://localhost:9000',
+        'https://api.coinmarketcap.com/',
+      ],
+    },
+  });
+
+  app.use(cookieParser());
+  app.use(cspSettings);
+  app.use(bodyParser.urlencoded({ extended: false }));
+};

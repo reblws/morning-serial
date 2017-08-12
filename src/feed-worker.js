9a23a665f5b5df0c75f802af5e5aa28614343568
Enrico B <rflnrclws@protonmail.com>
2017-08-12 11:39:35 -0400

    Clean up results logging

--- src/feed-worker.js
+++ src/feed-worker.js
@@ -6,6 +6,7 @@ const { valueSeq } = require('./utils');
 
 const sources = valueSeq(data);
 const sourceTypes = valueSeq(types);
+
 function updateAllFeeds(conn) {
   return Promise.all(sources.map(feed => updateFeed(conn, feed)));
 }
@@ -14,7 +15,9 @@ async function updateFeed(conn, feed) {
   try {
     const docs = await feed.listing;
     return db.updateTable(conn, feed.type, docs)
-      .then(results => console.log(results.inserted, 'Inserted', feed.type));
+      .then(results => {
+        console.log(results.inserted, 'Inserted', feed.type);
+      });
   } catch(e) {
     throw e;
   }

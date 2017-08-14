b926f9e576aa6c89bb03affc1059b15f1844576d
Enrico B <rflnrclws@protonmail.com>
2017-08-14 17:27:00 -0400

    Place console.log date in the right function

--- src/feed-worker.js
+++ src/feed-worker.js
@@ -8,13 +8,13 @@ const sources = valueSeq(data);
 const sourceTypes = valueSeq(types);
 
 function updateAllFeeds(conn) {
+  console.log(Date.now());
   return Promise.all(sources.map(feed => updateFeed(conn, feed)));
 }
 
 async function updateFeed(conn, feed) {
   try {
     const docs = await feed.listing;
-    console.log(Date.now());
     return db.updateTable(conn, feed.type, docs)
       .then(results => {
         console.log(results.inserted, 'Inserted', feed.type);

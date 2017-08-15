d677f49e47ce8df981445e06d9f5ce1a0c3f101e
Enrico B <rflnrclws@protonmail.com>
2017-08-15 14:18:09 -0400

    Clean up unused postcss config values

--- postcss.config.js
+++ postcss.config.js
@@ -1,43 +1,9 @@
 const path = require('path');
 
-const fontDir = path.join(__dirname, 'dist', 'assets', 'fonts');
-const hkGroteskPath = font => ({
-  url: {
-    otf: path.join(fontDir, `HKGrotesk-${font}.otf`),
-  },
-});
-const hkGroteskAllPaths = decoration => ({
-  '100': hkGroteskPath(`Light${decoration}`),
-  '300': hkGroteskPath(decoration !== 'Italic' ? 'Regular' : 'Italic'),
-  '400': hkGroteskPath(`Medium${decoration}`),
-  '700': hkGroteskPath(`SemiBold${decoration}`),
-  '900': hkGroteskPath(`Bold${decoration}`),
-});
-
 module.exports = {
   plugins: [
     require('postcss-import')(),
     require('postcss-cssnext')(),
     require('postcss-short')(),
-    // require('lost')(),
-    // require('postcss-font-magician')({
-    //   display: 'swap',
-    //   hosted: [fontDir, '/assets/fonts'],
-    //   formats: 'otf ttf',
-    //   foundries: ['google'],
-    //   variants: {
-    //     'Zilla Slab': {
-    //       '700': [],
-    //     },
-    //   },
-    //   custom: {
-    //     'HK Grotesk': {
-    //       variants: {
-    //         normal: hkGroteskAllPaths(''),
-    //         italic: hkGroteskAllPaths('Italic'),
-    //       },
-    //     },
-    //   },
-    // }),
   ],
 };

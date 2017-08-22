a7c77237dc34e93982f8310297693f6ce5c1be6a
Enrico B <rflnrclws@protonmail.com>
2017-08-22 13:17:55 -0400

    Add favicons

--- src/view/template.js
+++ src/view/template.js
@@ -7,6 +7,12 @@ module.exports = function html({ body, title, initialState, version = 0 }) {
       <head>
         <script>window.__APP_INITIAL_STATE__ = ${initialState}</script>
         <title>${title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+        <link rel="manifest" href="/assets/manifest.json">
+        <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="theme-color" content="#FAF9F0">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/assets/bundle.css?v=${version}" />
       </head>

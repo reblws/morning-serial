3c9785c05d6bd4a55e8cc0bbf214ecd249e0bb1e
Enrico B <rflnrclws@protonmail.com>
2017-08-29 12:47:17 -0400

    Set character encoding to utf-8 in template and set checksum to root react element

--- src/view/template.js
+++ src/view/template.js
@@ -1,16 +1,18 @@
 // src/template.js
 const React = require('react');
-const { template } = require('rapscallion');
+const { template, render } = require('rapscallion');
 const App = require('./index').default;
 
 
 module.exports = function html({ title, initialState, version = 0 }) {
-  const stringVersion = String(version);
+  const versionString = String(version);
+  const appRenderer = render(<App {...initialState} />);
   return template`
     <!DOCTYPE html>
     <html lang="en">
       <head>
-        <script>window.__APP_INITIAL_STATE__ = ${JSON.stringify(initialState)}</script>
+        <meta charset="utf-8">
+        <script>window.__APP_INITIAL_STATE__ = ${JSON.stringify(initialState)};</script>
         <title>${title}</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
@@ -19,12 +21,16 @@ module.exports = function html({ title, initialState, version = 0 }) {
         <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="theme-color" content="#FAF9F0">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/assets/bundle.css?v=${stringVersion}" />
+        <link rel="stylesheet" href="/assets/bundle.css?v=${versionString}" />
       </head>
 
       <body>
-        <div id="root">${<App {...initialState} />}</div>
-        <script src="/assets/bundle.js?v=${stringVersion}"></script>
+        <div id="root">${appRenderer}</div>
+        <script>
+          document.querySelector('main')
+            .setAttribute('data-react-checksum', ${() => appRenderer.checksum()});
+        </script>
+        <script src="/assets/bundle.js?v=${versionString}"></script>
       </body>
 
     </html>
